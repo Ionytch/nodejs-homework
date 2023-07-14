90a298227cc0242726a3e2dba82eef96ccb95e4f
Ionytch <starik.ionytch@gmail.com>
2023-07-15 01:36:57 +0300

    jimp added

--- controllers/auth/uploadImage.js
+++ controllers/auth/uploadImage.js
@@ -1,9 +1,11 @@
 const path=require('path');
 const fs=require('fs/promises')
 const User=require('../../models/user')
+const Jimp = require("jimp")
+
 
 const uploadImage=async(req, res)=>{
-    console.log ('user =', req.user)
+    // console.log ('user =', req.user)
     // const {_id}=req.user
     if(!req.file){
         res.status(400).json ({message:'file is required'})
@@ -15,12 +17,22 @@ const uploadImage=async(req, res)=>{
     const {filename}=req.file
     const tmpPath=path.resolve(__dirname,'../../tmp', filename)
     const publicPath=path.resolve(__dirname,'../../public/avatar', filename)
+    console.log(' name = ', filename)
 
+
+    await Jimp.read(tmpPath)
+    .then ((image) => {
+        return image.resize(250, 250).writeAsync(tmpPath) 
+           })
+      .catch((err) => {
+        console.error(err);
+      });
+
+    
     try {
         await fs.rename(tmpPath, publicPath)
     } catch (error) {
         await fs.unlink(tmpPath)
-        // res.status(400).json ({message:'file is required'})
         throw error
 
     }
@@ -28,12 +40,7 @@ const uploadImage=async(req, res)=>{
 const avatarURL=path.join('avatar', filename)
 await User.findByIdAndUpdate(req.user._id, {avatarURL})
 
-    // const userID=req.user.id
-    // const user=await User.findByIdAndUpdate(userID,
-    //     {image:`avatar/${filename}`,},
-    //     {new:true,},
-    //     )
-    
+     
     return res.json({avatarURL,})
 }
 
